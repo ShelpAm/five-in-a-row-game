6e0f117abcfba1d6341ff338f7335aeb70293015
small_sheep_ <1178550325@qq.com>
2023-04-25 22:01:09 +0800

    refactor: change ```application_is_on``` to ```state```

--- include/five_in_a_row_game/application.h
+++ include/five_in_a_row_game/application.h
@@ -5,26 +5,38 @@
 #ifndef FIVE_IN_A_ROW_GAME_APPLICATION_H_
 #define FIVE_IN_A_ROW_GAME_APPLICATION_H_
 
+#include <cstddef>
 #include <vector>
 
 #include "five_in_a_row_game/five_in_a_row_game.h"
 #include "five_in_a_row_game/player.h"
+
+enum class ApplicationState : std::size_t {
+  kApplicationStateStarted,
+  kApplicationStateStoped
+};
 
 class Application {
  public:
   Application();
 
   void MainLoop();
+
   void ParseCommand();
 
-  bool ApplicationIsOn() const { return application_is_on_; }
-  void SetApplicationIsOn(bool is_on) { application_is_on_ = is_on; }
-  int FramePerSecond() const { return frame_per_second_; }
+  void ProcessGameOver();
+
+  ApplicationState state() const { return state_; }
+  void set_state(ApplicationState state) { state_ = state; }
+
+  int frame_per_second() const { return frame_per_second_; }
 
  private:
+  void Update();
+
   int frame_per_second_{0};
   int last_frame_ = 0, current_frame_{0};
-  bool application_is_on_{false};
+  ApplicationState state_{ApplicationState::kApplicationStateStoped};
 
   std::vector<FiveInARowGame> game_histories_;
   FiveInARowGame game_{};
--- src/application.cc
+++ src/application.cc
@@ -15,24 +15,9 @@
 Application::Application() {}
 
 void Application::MainLoop() {
-  auto ProcessGameOver = [this]() -> void {
-    std::cout << "Game over!\n";
-    if (game_.winner()) {
-      std::cout << "The winner is " << game_.winner()->name() << '\n';
-    } else {
-      std::cout << "The game drew! No one wins!\n";
-    }
-    // game_.CleanUp();
-    game_ = FiveInARowGame();
-    ParseCommand();
-  };
-
-  SetApplicationIsOn(true);
-  while (ApplicationIsOn()) {
-    last_frame_ = current_frame_;
-    current_frame_ = static_cast<int>(clock());
-    frame_per_second_ = current_frame_ - last_frame_;
-
+  set_state(ApplicationState::kApplicationStateStarted);
+  while (state() == ApplicationState::kApplicationStateStarted) {
+    Update();
     switch (game_.game_state()) {
       case GameState::kGameStateNotStarted:
         ParseCommand();
@@ -40,7 +25,7 @@ void Application::MainLoop() {
       case GameState::kGameStateStarted:
         game_.Tick();
         continue;
-      case GameState::kGameStateOver:
+      case GameState::kGameStateStoped:
         ProcessGameOver();
         break;
     }
@@ -52,7 +37,7 @@ void Application::ParseCommand() {
   std::cout << "Please input a command:\n";
   std::cin >> command;
   if (command == "quit") {
-    this->SetApplicationIsOn(false);
+    this->set_state(ApplicationState::kApplicationStateStoped);
   } else if (command == "start-a-local-game" || command == "game") {
     HumanPlayerFactory human_player_factory;
     EasyAIPlayerFactory easy_ai_player_factory;
@@ -68,4 +53,22 @@ void Application::ParseCommand() {
     // game_.Start(human_player, ai_player);
     game_.Start(*ai_player1, *ai_player2);
   }
+}
+
+void Application::ProcessGameOver() {
+  std::cout << "Game over!\n";
+  if (game_.winner()) {
+    std::cout << "The winner is " << game_.winner()->name() << '\n';
+  } else {
+    std::cout << "The game drew! No one wins!\n";
+  }
+  // game_.CleanUp();
+  game_ = FiveInARowGame();
+  ParseCommand();
+}
+
+void Application::Update() {
+  last_frame_ = current_frame_;
+  current_frame_ = static_cast<int>(clock());
+  frame_per_second_ = current_frame_ - last_frame_;
 }
